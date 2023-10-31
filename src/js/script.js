9dbfc2f6450ffd4b315c57db34913506ca8fa4d8
Sai Khant Zay Lynn Yaung <91935626+Khant-SoDOpe@users.noreply.github.com>
2023-10-31 16:15:51 +0630

    update

--- src/js/script.js
+++ src/js/script.js
@@ -1,11 +1,14 @@
+// Call the function when the page loads to display all data initially
+fetchDataAndDisplayAllData(document.getElementById("output"));
+
 function searchNRC() {
   var input = document.getElementById("input").value;
   var output = document.getElementById("output");
 
   // Check if the input is empty or contains only whitespace
   if (input.trim() === "") {
-    // Clear the output when there's no input
-    output.innerHTML = "";
+    // Display all data when there's no input
+    fetchDataAndDisplayAllData(output);
     return;
   }
 
@@ -19,7 +22,7 @@ function searchNRC() {
         output.innerHTML = "<div class='scrollable-table'><table><tr><th>NRC Code</th><th>NRC Name</th><th>City Name</th></tr>" + matchingInfo.join("") + "</table></div>";
       } else {
         // Clear the output when there are no matching entries
-        output.innerHTML =  "No matching entry found.";
+        output.innerHTML = "No matching entry found.";
       }
     })
     .catch(error => {
@@ -28,10 +31,24 @@ function searchNRC() {
     });
 }
 
-    function extractTextWithinParentheses(text) {
-      var matches = text.match(/\((.*?)\)/);
-      if (matches && matches.length > 0) {
-        return matches[1];
-      }
-      return "";
-    }
+function fetchDataAndDisplayAllData(output) {
+  fetch('src/data/data.json')
+    .then(response => response.json())
+    .then(data => {
+      var entries = data.data;
+      var allData = entries.map(e => "<tr><td>" + e.nrc_code + "</td><td>" + extractTextWithinParentheses(e.name_mm) + "</td><td>" + e.city_name + "</td></tr>");
+      output.innerHTML = "<div class='scrollable-table'><table><tr><th>NRC Code</th><th>NRC Name</th><th>City Name</th></tr>" + allData.join("") + "</table></div>";
+    })
+    .catch(error => {
+      console.log('Error:', error);
+      output.innerHTML = "Error fetching data.";
+    });
+}
+
+function extractTextWithinParentheses(text) {
+  var matches = text.match(/\((.*?)\)/);
+  if (matches && matches.length > 0) {
+    return matches[1];
+  }
+  return "";
+}
